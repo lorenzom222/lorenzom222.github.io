eeb2540f0cc5e90e4b15c399125f969c39a22a5b
lorenzom222 <lorenzomwork@gmail.com>
2024-07-20 15:55:30 -0700

    fixed the birds around and made them as a child to profile instead of the whole thing

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+let profileSection = document.querySelector('#profile');
+let vantaContainer = document.createElement('div');
+vantaContainer.classList.add('vanta-container');
+profileSection.appendChild(vantaContainer);
+
+let vantaEffect = VANTA.BIRDS({
+    el: vantaContainer,
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: profileSection.offsetHeight,
+    minWidth: window.innerWidth, // Full viewport width
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xffffff,
+    speedLimit: 3.00,
+    separation: 14.00
+});
